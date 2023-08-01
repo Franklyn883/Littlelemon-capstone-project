8bf5aa832e779b8fab87c9485145dbce6833d722
Frank Alimimian <alimimianfrank@gmail.com>
2023-08-01 13:57:28 +0100

    Security: Add python-dotenv to secure sensitive credentials

--- Littlelemon/settings.py
+++ Littlelemon/settings.py
@@ -15,12 +15,19 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#setting up environment variable with python-dotenv
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7l(ykd=o$w$w!=(hpsoi6w(-n4ggg15oa2+pe^f#vy6sw^51kx'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,8 +85,13 @@ WSGI_APPLICATION = 'Littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('PASSWORD'),
+        'HOST': os.environ.get('HOST'),
+        'PORT': os.environ.get('PORT'),
+        'OPTIONS': { 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
 }
 
